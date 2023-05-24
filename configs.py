0589c31bf314473ac9f77723fb973e3e320423b3
thenmmovies <97659554+thenmmovies@users.noreply.github.com>
2023-05-24 14:58:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,10 +3,10 @@
 class Config:
     TG_API_ID = 28123504
     TG_API_HASH = "266272106c7fb627c1753f776e15e498"
-    TG_BOT_TOKEN = "5939918113:AAHa9j6QkLbuM1ZnUavsJ0v7GINroRBzsZE"
+    TG_BOT_TOKEN = "1932680556:AAGatbrPIFEfrRooRawLXsEhDbcf3khmw_o"
     ADMIN_IDS = ["Nmfajis"]
     DUMP_CHANNEL_ID = -1001940823180
-    MONGO_DB_ACCESS_URI = "mongodb+srv://usr:123@cluster0.ftt6apw.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_ACCESS_URI = "mongodb+srv://mhdfajisnm:OyOrQYN1LGvFB4Am@cluster0.syr8n2p.mongodb.net/?retryWrites=true&w=majority"
     MONGO_DB_COLLECTION_NAME = "DB-01"
     SMM_PANEL_API_KEY = "87752e15bbb34b11b8cdb8fde2f7acde"
 
